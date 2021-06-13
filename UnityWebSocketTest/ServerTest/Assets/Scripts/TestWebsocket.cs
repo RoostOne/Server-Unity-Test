db8067912f219b0f16c8f991d7e500449b1424f2
torsten Hahn <torsten.hahn123@gmail.com>
2021-06-13 18:39:04 +0200

    init

---
+++ UnityWebSocketTest/ServerTest/Assets/Scripts/TestWebsocket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestWebsocket : MonoBehaviour
+{
+    public Text TextServer;
+    public Button ButtonServer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonServer.onClick.AddListener(setText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setText(){
+        TextServer.text = "Button pressed";
+    }   
+}
